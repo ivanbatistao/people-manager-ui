ea1185414a68b3adb890b959da9236031c66d283
Ivan E. Batista Ochoa <batistaochoaivan@gmail.com>
2025-07-21 21:07:53 -0500

    style: add styled-components theme and base styles

---
+++ src/styled-components/layout.styled.component.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.div`
+    width: fit-content;
+
+    @media only screen and (max-width: 600px) {
+        width: 80vw;
+        padding: 12px;
+    }
+`;
